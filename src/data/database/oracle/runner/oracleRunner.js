fb9f130c416bab6323fb527dc9abfd92261a516a
Diego Flores <ext_dfloresr@falabella.cl>
2021-09-30 00:33:55 -0300

    Re deploy

--- src/data/database/oracle/runner/oracleRunner.js
+++ src/data/database/oracle/runner/oracleRunner.js
@@ -1,10 +1,10 @@
-'use strict'
-const oracledb = require('oracledb')
-const RunnerStrategy = require('./runnerStrategy')
+'use strict';
+const oracledb = require('oracledb');
+const RunnerStrategy = require('./runnerStrategy');
 
 class OracleRunner extends RunnerStrategy {
 	constructor() {
-		super()
+		super();
 	}
 
 	async runProcedure(procedure) {
@@ -14,53 +14,52 @@ class OracleRunner extends RunnerStrategy {
 				user: process.env.ORACLE_DB_USER,
 				password: process.env.ORACLE_DB_PASSWORD,
 				connectString: process.env.ORACLE_DB_HOST
-			})
+			});
 
-			logger.info(`Ejecutando procedimiento ${procedure.statement}`)
+			logger.info(`Ejecutando procedimiento ${procedure.statement}`);
 			const result = await connection.execute(procedure.statement, procedure.bind, {
 				outFormat: oracledb.OUT_FORMAT_OBJECT
-			})
-			logger.info(`Procedimiento ${procedure.name} ejecutado exitosamente`)
-			await connection.close()
-			return result
+			});
+			logger.info(`Procedimiento ${procedure.name} ejecutado exitosamente`);
+			await connection.close();
+			return result;
 		} catch (error) {
-			logger.error(error)
-			throw { httpCode: 503, code: error.errorNum.toString(), message: 'Servicio no disponible' }
+			logger.error(error);
+			throw { httpCode: 503, code: error.errorNum.toString(), message: 'Servicio no disponible' };
 		}
 	}
 
 	async runCursorProcedure(procedure) {
 		try {
-			// TODO Validar donde colocar conexion a base de datos
 			const connection = await oracledb.getConnection({
 				user: process.env.ORACLE_DB_USER,
 				password: process.env.ORACLE_DB_PASSWORD,
 				connectString: process.env.ORACLE_DB_HOST
-			})
+			});
 
-			logger.info(`Ejecutando procedimiento ${procedure.statement}`)
+			logger.info(`Ejecutando procedimiento ${procedure.statement}`);
 			const result = await connection.execute(procedure.statement, procedure.bind, {
 				prefetchRows: 1000,
 				fetchArraySize: 1000,
 				outFormat: oracledb.OUT_FORMAT_OBJECT
-			})
-			logger.info(`Procedimiento ${procedure.name} ejecutado exitosamente`)
+			});
+			logger.info(`Procedimiento ${procedure.name} ejecutado exitosamente`);
 
-			const resultSet = result.outBinds.P_RECORDSET
-			const arrayResult = []
-			let row
+			const resultSet = result.outBinds.P_RECORDSET;
+			const arrayResult = [];
+			let row;
 			while ((row = await resultSet.getRow())) {
-				arrayResult.push(row)
+				arrayResult.push(row);
 			}
 
-			resultSet.close()
-			connection.close()
-			return arrayResult
+			resultSet.close();
+			connection.close();
+			return arrayResult;
 		} catch (error) {
-			logger.error(error)
-			throw { httpCode: 503, code: error.errorNum.toString(), message: 'Servicio no disponible' }
+			logger.error(error);
+			throw { httpCode: 503, code: error.errorNum.toString(), message: 'Servicio no disponible' };
 		}
 	}
 }
 
-module.exports = OracleRunner
+module.exports = OracleRunner;
