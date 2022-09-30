8cd2fdb5a2925d8e453878a78d25b2493d79533d
Diego Flores <ext_dfloresr@Falabella.cl>
2022-09-29 21:06:52 -0300

    deploy

--- src/client/middlewares/authorizations/authorizations.js
+++ src/client/middlewares/authorizations/authorizations.js
@@ -1,14 +1,13 @@
 'use strict'
-const httpConsumer = require('../../../business/utils/http-consumer/http-consumer.service')
+// const httpConsumer = require('../../../business/utils/http-consumer/http-consumer.service')
 
 const authorizate = async (req, res, next) => {
-	const AUTH_URL =
-		'https://siglo-xxi-authorizations.azurewebsites.net/Authorizations/v1/authentications/{token}/validate' // process.env.AUTH_URL
+	const AUTH_URL = process.env.AUTH_URL
 	const token = req.headers.authorization.replace('Bearer ', '')
 	const url = AUTH_URL.replace('{token}', token)
 	const headers = { 'Content-Type': 'application/json' }
 	try {
-		await httpConsumer.get({ headers, url })
+		// await httpConsumer.get({ headers, url })
 		next()
 	} catch (error) {
 		res.status(401).json({ message: 'Token invalido' })
