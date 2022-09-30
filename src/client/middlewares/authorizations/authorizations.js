85e2a364d8203efedb3ee9e3153e14872ee0d76d
Diego Flores <ext_dfloresr@Falabella.cl>
2022-09-29 21:36:34 -0300

    deploy

--- src/client/middlewares/authorizations/authorizations.js
+++ src/client/middlewares/authorizations/authorizations.js
@@ -1,18 +1,17 @@
 'use strict'
-// const httpConsumer = require('../../../business/utils/http-consumer/http-consumer.service')
+const httpConsumer = require('../../../business/utils/http-consumer/http-consumer.service')
 
 const authorizate = async (req, res, next) => {
-	// const AUTH_URL = process.env.AUTH_URL
+	const AUTH_URL = process.env.AUTH_URL
 	// const token = req.headers.authorization.replace('Bearer ', '')
-	// const url = AUTH_URL.replace('{token}', token)
-	// const headers = { 'Content-Type': 'application/json' }
-	// try {
-	// 	await httpConsumer.get({ headers, url })
-	// 	next()
-	// } catch (error) {
-	// 	res.status(401).json({ message: 'Token invalido' })
-	// }
-	next()
+	const url = AUTH_URL.replace('{token}', token)
+	const headers = { 'Content-Type': 'application/json' }
+	try {
+		//await httpConsumer.get({ headers, url })
+		next()
+	} catch (error) {
+		res.status(401).json({ message: 'Token invalido' })
+	}
 }
 
 module.exports = { authorizate }
