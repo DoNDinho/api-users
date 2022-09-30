be6d84c21601a9e5b3f08913589e82a612dfe03f
Diego Flores <ext_dfloresr@Falabella.cl>
2022-09-29 21:02:19 -0300

    deploy

--- src/client/middlewares/authorizations/authorizations.js
+++ src/client/middlewares/authorizations/authorizations.js
@@ -2,7 +2,8 @@
 const httpConsumer = require('../../../business/utils/http-consumer/http-consumer.service')
 
 const authorizate = async (req, res, next) => {
-	const AUTH_URL = process.env.AUTH_URL
+	const AUTH_URL =
+		'https://siglo-xxi-authorizations.azurewebsites.net/Authorizations/v1/authentications/{token}/validate' // process.env.AUTH_URL
 	const token = req.headers.authorization.replace('Bearer ', '')
 	const url = AUTH_URL.replace('{token}', token)
 	const headers = { 'Content-Type': 'application/json' }
